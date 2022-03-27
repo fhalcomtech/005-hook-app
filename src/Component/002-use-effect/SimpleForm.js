fdd9d7fbcd5610b3323d58ec8abf210261e71a46
Fidel Lascano <fidel.lascano@conauto.com.ec>
2022-03-27 17:43:43 -0500

    Esto pinta bien

---
+++ src/Component/002-use-effect/SimpleForm.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+
+export const SimpleForm = () => {
+    const [formData, setFormData] = useState({user: '', email:''});
+    const {user,email} = formData;
+    const handlerChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value})
+    }
+
+    useEffect(()=>{console.log('Inicio el componente')},[])
+
+    useEffect(()=>{
+        setFormData({...formData, email: user.length})
+    },[user]);
+
+    return (
+        <>
+            <h1>SimpleForm Login</h1>
+            <div className = "container">
+            <input 
+            type="text"
+            name="user"
+            placeholder='write here'
+            className="form-control"
+            value={user}
+            onChange={handlerChange}
+            >
+            </input>
+            
+
+<input 
+            type="email"
+            name="email"
+            placeholder=''
+            value={email}
+            className="form-control mt-2"
+            onChange={handlerChange}
+            ></input>
+            </div>
+        </>
+  )
+}
--- src/HookApp.js
+++ src/HookApp.js
@@ -1,7 +1,8 @@
 import { CounterApp } from "./Component/001-use-state/CounterApp";
 import { CounterWithCustomHook } from "./Component/001-use-state/CounterWithCustomHook";
+import { SimpleForm } from "./Component/002-use-effect/SimpleForm";
 
 const HookApp = () => (<>
-        <CounterWithCustomHook/>
+        <SimpleForm/>
     </>);
 export default HookApp;
